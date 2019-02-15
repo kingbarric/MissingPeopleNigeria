1771e2707fd8f949bfa9e73e0850058e98043aef
Eric <30669026+educare-mit@users.noreply.github.com>
2019-02-15 12:39:56 +0100

    ok

--- src/providers/pages-services-crud-service/pages-services-crud-service.ts
+++ src/providers/pages-services-crud-service/pages-services-crud-service.ts
@@ -11,7 +11,7 @@ import { Injectable } from '@angular/core';
 export class PagesServicesCrudServiceProvider {
 
   constructor(public http: HttpClient) {
-    console.log('Hello PagesServicesCrudServiceProvider Provider');
+    console.log('/oh');
   }
 
 }
